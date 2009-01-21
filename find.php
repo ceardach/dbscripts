1f63849c8f1882a460ab7da91d9362dff0956e39
Kathleen Murtagh <kathleen@ceardach.com>
2009-01-21 12:34:18 -0500

    Changing find increment output to an array so it can be used

--- find.php
+++ find.php
@@ -8,7 +8,8 @@
     if (isset($options[1]) && $options[1] == 'increment') {
       $branch = isset($options[2]) ? $options[2] : 'development';
       $filter_option = isset($options[3]) ? $options[3] : 'full';
-      print dbscripts_tables_with_increment($branch, $filter_option);
+      $list = dbscripts_tables_with_increment($branch, $filter_option);
+      print_r($list);
     } else {
     	print "\nNot a valid search.\n\n";
     }
