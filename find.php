f35b9f606f94a6ff4642e611c7fa6aa192db0c42
ceardach <ceardach>
2009-02-22 21:58:49 +0000

    Adding ability to return all references to a given table.

--- find.php
+++ find.php
@@ -6,17 +6,19 @@
   } else {
     $options = $_SERVER['argv'];
 
+    // Find all tables with auto_increment
     if (isset($options[1]) && $options[1] == 'increment') {
       $branch = isset($options[2]) ? $options[2] : 'development';
       $filter_option = isset($options[3]) ? $options[3] : 'full';
       $list = dbscripts_find_tables_with_increment($branch, $filter_option);
       print_r($list);
 
-    } elseif (isset($options[1]) && $options[1] == 'references') {
+    // Find all possible references to a given table
+    } elseif (isset($options[1]) && $options[1] == 'possible-references') {
+      print "\n Please wait. This can take awhile.  'ctrl+c' to cancel.\n\n";
+
       $branch = isset($options[3]) ? $options[3] : 'development';
       $filter_option = isset($options[4]) ? $options[4] : 'full';
-
-      print "\n Please wait. This can take awhile.  'ctrl+c' to cancel.\n\n";
 
       $list = array();
     	if(isset($options[2]) && $options[2] != 'all') {
@@ -24,15 +26,33 @@
         $list = dbscripts_find_possible_table_references($table, $branch, $filter_option);
     	} else {
         $table_list = dbscripts_find_tables_with_increment($branch, $filter_option);
-        foreach ($table_list as $table) {
-        	$references = dbscripts_find_possible_table_references($table, $branch, $filter_option);
-          if ($references) {
-          	$list[$table] = $references;
+        if (is_array($table_list)) {
+          foreach ($table_list as $table) {
+          	$references = dbscripts_find_possible_table_references($table, $branch, $filter_option);
+            if (is_array($references)) {
+            	$list[$table] = $references;
+            }
           }
+        } else {
+          // If not an array, pass through the error message
+        	print "\n$table_list\n\n";
         }
     	}
       print_r($list);
 
+    // Find all configured and possible references to a given table
+    } elseif (isset($options[1]) && $options[1] == 'references') {
+      if(isset($options[2])) {
+        $table = $options[2];
+        $branch = isset($options[3]) ? $options[3] : 'development';
+        $filter_option = isset($options[4]) ? $options[4] : 'full';
+        $references = dbscripts_get_table_references($table, $branch, $filter_option);
+        print_r($references);
+      } else {
+      	print "\nMust provide a table to check.\n\n";
+      }
+
+    // Fail
     } else {
     	print "\nNot a valid search.\n\n";
     }
