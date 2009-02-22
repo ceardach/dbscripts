cc88b416bd5ac84ddc3497228145b49ebc022a82
ceardach <ceardach>
2009-02-22 22:15:35 +0000

    Fixing some bugs, and optimizing user abilities.

--- dump.php
+++ dump.php
@@ -5,6 +5,6 @@
     print dbscripts_help('dump');
   } else {
     $options = dbscripts_get_options($_SERVER['argv']);
-    print dbscripts_dump($options['file'],$options['filter']);
+    print dbscripts_dump($options['branch'], $options['filter'], $options['last-merge']);
   }
 ?>
--- raise_increments.php
+++ raise_increments.php
@@ -1,18 +1,25 @@
 #!/usr/bin/env php
 <?php
   require('dbscripts.module');
-  $options = $_SERVER['argv'];
-
-  if ($options[1] == 'all') {
-    print dbscripts_raise_all_increments();
+  if (in_array('help', $_SERVER['argv'])) {
+    print dbscripts_help('raise_increments');
   } else {
-    $table = $options[1];
-    $column = $options[2];
-    $start_at = $options[3];
-    $change_to = $options[4];
-    $branch = $options[5];
-    $filter_option = $options[6];
+    $options = $_SERVER['argv'];
 
-    print dbscripts_raise_table_increments($table, $column, $start_at, $change_to, $branch, $filter_option);
+    if (isset($options[1]) && $options[1] != 'all') {
+      if (isset($options[4])) {
+        $table = $options[1];
+        $start_at = $options[3];
+        $change_to = $options[4];
+        $branch = $options[5];
+        $filter_option = $options[6];
+
+        print dbscripts_raise_table_increments($table, $start_at, $change_to, $branch, $filter_option);
+      } else {
+        print "\n\nMust provide a table, increment to start at, and increment to change to\n\n";
+      }
+    } else {
+      print dbscripts_raise_all_increments();
+    }
   }
 ?>
