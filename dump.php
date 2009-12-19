90a1ff15365e96e04cdf54b8b852a07d16d319f2
Fox <fox@Alexander-Grape-labtop.local>
2009-12-18 19:43:37 -0500

    Begining support for the -fc== option to change the configuration option

--- dump.php
+++ dump.php
@@ -5,6 +5,6 @@
     print dbscripts_help('dump');
   } else {
     $options = _dbscripts_get_options($_SERVER['argv']);
-    print dbscripts_dump($options['branch'], $options['filter'], $options['last-merge']);
+    print dbscripts_dump($options);
   }
 ?>
